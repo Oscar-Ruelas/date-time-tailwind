377e9deb9f287e289498a63801d4cfb3fa8856a2
Oscar Ruelas <114115100+Oscar-Ruelas@users.noreply.github.com>
2024-12-04 18:41:32 +0000

    made some changes

--- public/time.js
+++ public/time.js
@@ -2,19 +2,24 @@ const theClockApp = () => {
     const timeNow = new Date();
     let gotHours = timeNow.getHours();
     let gotMinutes = timeNow.getMinutes();
-    
+    let gotSeconds = timeNow.getSeconds();
+
     const gotAmOrPM = gotHours >= 12 ? 'PM' : 'AM';
     gotHours = gotHours % 12 || 12;
-    
+
     const hoursTens = Math.floor(gotHours / 10);
     const hoursUnits = gotHours % 10;
     const minutesTens = Math.floor(gotMinutes / 10);
     const minutesUnits = gotMinutes % 10;
+    const secondsTens = Math.floor(gotSeconds / 10);
+    const secondsUnits = gotSeconds % 10;
 
     const hoursTensElem = document.querySelector(".hours.tens");
     const hoursUnitsElem = document.querySelector(".hours.units");
     const minutesTensElem = document.querySelector(".minutes.tens");
     const minutesUnitsElem = document.querySelector(".minutes.units");
+    const secondsTensElem = document.querySelector(".seconds.tens");
+    const secondsUnitsElem = document.querySelector(".seconds.units");
     const amOrPMElem = document.querySelector(".amOrPM");
 
     // Update hours tens place
@@ -45,12 +50,26 @@ const theClockApp = () => {
         setTimeout(() => minutesUnitsElem.classList.remove('animate-flip'), 600);
     }
 
+    // Update seconds tens place
+    if (secondsTensElem.innerText !== `${secondsTens}`) {
+        secondsTensElem.innerText = secondsTens;
+        secondsTensElem.classList.add('animate-flip');
+        setTimeout(() => secondsTensElem.classList.remove('animate-flip'), 600);
+    }
+
+    // Update seconds units place
+    if (secondsUnitsElem.innerText !== `${secondsUnits}`) {
+        secondsUnitsElem.innerText = secondsUnits;
+        secondsUnitsElem.classList.add('animate-flip');
+        setTimeout(() => secondsUnitsElem.classList.remove('animate-flip'), 600);
+    }
+
     // Update AM or PM
     if (amOrPMElem.innerText !== gotAmOrPM) {
         amOrPMElem.innerText = gotAmOrPM;
         amOrPMElem.classList.add('animate-flip');
         setTimeout(() => amOrPMElem.classList.remove('animate-flip'), 600);
     }
-}
+};
 
 setInterval(theClockApp, 1000);
